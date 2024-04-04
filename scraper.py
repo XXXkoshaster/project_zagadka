2701f7d2e8c7075c46a1f5ab799f40cee6890244
xxxkoshaster <koshaster13@gmail.com>
2024-04-04 03:12:11 +0300

    added new subclass VkGroups to get information about groups

--- scraper.py
+++ scraper.py
@@ -20,23 +20,35 @@ class VkProfile:
         self.session = vk_api.VkApi(token=token)
         self.api = self.session.get_api()
 
+#подкласс VkProfile для получения информации о самом пользователе
 class VkUser(VkProfile):
     def get_user_info(self, user_id):
         try:
-            return self.api.users.get(user_ids=user_id, fields="bdate, city, country")
+            return self.api.users.get(user_ids=user_id, fields="sex, bdate, city, country")
         except vk_api.ApiError as e:
             print(f"Ошибка API вконтакте при запросе информации о пользователе: {e}")
         except Exception as e:
             print(f"Произошла ошибка при запросе информации о пользователе: {e}")
 
+#подкласс VkProfile для получения информации о друзьях пользователя
 class VkFriends(VkProfile):
     def get_friends_info(self, user_id):
         try:
-            return self.api.friends.get(user_id=user_id, fields="bdate, city, country")
+            return self.api.friends.get(user_id=user_id, fields="sex, bdate, city, country")
         except vk_api.ApiError as e:
             print(f"Ошибка API вконтакте при запросе информации о друзьх пользователя: {e}")
         except Exception as e:
             print(f"Произошла ошибка при запросе информации о друзьх пользователя: {e}")
+
+#подкласс VkProfile для получения информации о друзьях пользователя
+class VkGroups(VkProfile):
+    def get_groups_info(self, user_id):
+        try:
+            return self.api.groups.get(user_id=user_id, extended=1, fields="activity, city, country, site")
+        except vk_api.ApiError as e:
+            print(f"Ошибка API вконтакте при запросе информации о сообщестах пользователя: {e}")
+        except Exception as e:
+            print(f"Произошла ошибка при запросе информации о сообществах пользователя: {e}")
 
 #класс для сохранения инфромации о пользователе в файл json
 class File:
@@ -53,6 +65,7 @@ class VkApp:
         self.token = os.getenv("API_KEY")
         self.user = VkUser(self.token)
         self.friends = VkFriends(self.token)
+        self.groups = VkGroups(self.token)
 
     def run(self):
         url = input("Введите URL профиль Вконтакте: ")
@@ -62,11 +75,11 @@ class VkApp:
         if user_id:
             user_data = self.user.get_user_info(user_id)
             friends_data = self.friends.get_friends_info(user_id)
+            groups_data = self.groups.get_groups_info(user_id)
             user_data[0]["URL"] = url
             File.save_data("user_data.json", user_data[0])
-            File.save_data("friends_data.json", friends_data)
-            print(type(friends_data), type(user_data[0]))
-
+            File.save_data("friends_data.json", friends_data["items"])
+            File.save_data("groups_data.json", groups_data)
 
 #объединение в общий скрипт
 if __name__ == "__main__":
