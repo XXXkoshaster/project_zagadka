36025b27730007a1065442b272c3f24e9cd9c227
alina <chokuraevalina@mail.ru>
2024-04-16 19:27:13 +0300

    changes for correct working and chandes of font size

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import dash
 import json
 from dash import html 
 
-with open("data.jason") as f:
+with open("data.json") as f:
     data = json.load(f)
 
 app = dash.Dash(__name__)
@@ -12,20 +12,20 @@ app.layout = html.Div([
         style={"color": "blue",
                "fontSize": "40px"}),
     html.H2("VK scraper"),
-    html.P("Data user:"),
+    html.P("Data user:", style={"fontSize": "24px"}),
     html.Ul([
         html.Li(f"ID of user: {data['id']}"),
-        html.Li(f"Birthday date of user: {data['date']}"),
-        html.Li(f"City of user: {data['city']}"),
-        html.Li(f"Country of user: {data['country']}"),
+        html.Li(f"Birthday date of user: {data['bdate']}"),
+        html.Li(f"City of user: {data['city']['title']}"),
+        html.Li(f"Country of user: {data['country']['title']}"),
         html.Li(f"First name of user: {data['first_name']}"),
         html.Li(f"Last name of user: {data['last_name']}"),
-        html.Li(f"Is accaunt of user closed: {data['is_closed']}"),
+        html.Li(f"Accaunt of user is closed: {data['is_closed']}"),
         html.Li([
-            "User account ",
-            html.A(data["URL"])
+            "URL of account: ",
+            html.A(data["URL"], href=data["URL"])
         ])
-    ])
+    ], style={"fontSize": "20px"})
 ])
 
 if __name__ == "__main__":
