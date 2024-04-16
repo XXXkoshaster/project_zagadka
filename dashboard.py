634318214f858e41fce564d304209cdc6e231f1b
alina <chokuraevalina@mail.ru>
2024-04-16 19:30:54 +0300

    added tabs and text with info about project

--- dashboard.py
+++ dashboard.py
@@ -7,29 +7,38 @@ with open("data.json") as f:
     data = json.load(f)
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-dbc.Row([
-    dbc.Col('Column 1', width=2),
-    dbc.Col('Column 2', width=5),
-    dbc.Col('Column 3', width=4),
-])
+
 app.layout = html.Div([
     html.H1(children="PROJECT ZAGADKA",
         style={"color": "blue",
                "fontSize": "40px"}),
     html.H2("VK scraper"),
-    html.P("Data user:", style={"fontSize": "24px"}),
-    html.Ul([
-        html.Li(f"ID of user: {data['id']}"),
-        html.Li(f"Birthday date of user: {data['bdate']}"),
-        html.Li(f"City of user: {data['city']['title']}"),
-        html.Li(f"Country of user: {data['country']['title']}"),
-        html.Li(f"First name of user: {data['first_name']}"),
-        html.Li(f"Last name of user: {data['last_name']}"),
-        html.Li(f"Accaunt of user is closed: {data['is_closed']}"),
-        html.Li([
-            "URL of account: ",
-            html.A(data["URL"], href=data["URL"])
+
+    dbc.Tabs([
+        dbc.Tab([
+            html.Ul([
+                html.Br(),
+                html.Li(f"ID of user: {data['id']}"),
+                html.Li(f"Birthday date of user: {data['bdate']}"),
+                html.Li(f"City of user: {data['city']['title']}"),
+                html.Li(f"Country of user: {data['country']['title']}"),
+                html.Li(f"First name of user: {data['first_name']}"),
+                html.Li(f"Last name of user: {data['last_name']}"),
+                html.Li(f"Accaunt of user is closed: {data['is_closed']}"),
+                html.Li([
+                    "URL of account: ",
+                    html.A(data["URL"], href=data["URL"])
+            ])
         ])
+    ], label="Data user"),
+        dbc.Tab([
+            html.Br(),
+            html.P("Web server for analising data vk users."),
+            html.P(["Git hub repo: ",
+                    html.A('https://github.com/XXXkoshaster/project_zagadka', href='https://github.com/XXXkoshaster/project_zagadka')
+                ])
+        ], label='Project info')
+    
     ], style={"fontSize": "20px"})
 ])
 
